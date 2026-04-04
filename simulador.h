55c33f0be06b2a63453e898d729cd4871e685c50
João Vitor Munhoz Teixeira <135624403+jota6k@users.noreply.github.com>
2026-04-04 20:02:16 -0300

    Refactor simulator header for better structure

--- simulador.h
+++ simulador.h
@@ -1,8 +1,7 @@
-#define BIBLIOTECA_H
+#ifndef SIMULADOR_H
+#define SIMULADOR_H
 
-// Estrutura de dados das instrucoes
-
-struct instrucoes {
+struct decode {
     int opcode;
     int rs;
     int rt;
@@ -12,16 +11,30 @@ struct instrucoes {
     int addr;
 };
 
+struct EstadoMaquina {
+    int PC;
+    int registradores[8];
+    int memoria_dados[256];
+};
+
 void leitura_arquivo_mem(int memoria[]);
 void leitura_arquivos_dados(int memoria_dados[]);
 void inicializar_registradores(int registradores[]);
 int ler_reg(int registradores[], int indice);
 void escrever_reg(int registradores[], int indice, int valor);
+
 int fetch(int memoria_instrucao[], int PC);
 struct decode campos(int instrucao);
 void execute(struct decode c, int registradores[], int memoria_dados[], int *PC, int *aritmeticas, int *memoria_acesso);
 void executar_programa(int memoria_instrucao[], int memoria_dados[], int registradores[]);
 int ULA(int A, int B, int controle, int *flag);
+
 void imprimir_memoria(int memoria[]);
 void salvar_arquivo_dat(int memoria_dados[]);
 void salvar_asm(int memoria[]);
+
+void step(int memoria_instrucao[], int memoria_dados[], int registradores[], int *PC, int *arit, int *mem);
+void back(int registradores[], int memoria_dados[], int *PC);
+void run(int memoria_instrucao[], int memoria_dados[], int registradores[], int *PC, int *arit, int *mem);
+
+#endif
