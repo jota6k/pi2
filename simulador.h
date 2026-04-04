38a8d04d532030a169651ac56c69006a199125cd
João Vitor Munhoz Teixeira <135624403+jota6k@users.noreply.github.com>
2026-04-03 22:36:04 -0300

    Add new functions for data handling and execution

--- simulador.h
+++ simulador.h
@@ -13,9 +13,15 @@ struct instrucoes {
 };
 
 void leitura_arquivo_mem(int memoria[]);
+void leitura_arquivos_dados(int memoria_dados[]);
 void inicializar_registradores(int registradores[]);
 int ler_reg(int registradores[], int indice);
 void escrever_reg(int registradores[], int indice, int valor);
-void executar_programa(int memoria[], int registradores[]);
+int fetch(int memoria_instrucao[], int PC);
+struct decode campos(int instrucao);
+void execute(struct decode c, int registradores[], int memoria_dados[], int *PC, int *aritmeticas, int *memoria_acesso);
+void executar_programa(int memoria_instrucao[], int memoria_dados[], int registradores[]);
+int ULA(int A, int B, int controle, int *flag);
 void imprimir_memoria(int memoria[]);
+void salvar_arquivo_dat(int memoria_dados[]);
 void salvar_asm(int memoria[]);
