4f476cf863f1ac3b1a25f0898d786c439c1a5c5e
João Vitor Munhoz Teixeira <135624403+jota6k@users.noreply.github.com>
2026-04-14 13:10:20 -0300

    Refactor function declarations in simulador.h

--- simulador.h
+++ simulador.h
@@ -16,23 +16,20 @@ struct EstadoMaquina {
     int registradores[8];
     int memoria_dados[256];
 };
-void imprimir_memoria_instrucoes(int memoria[]);
-void imprimir_memoria_dados(int memoria[]);
-void escolher_arquivo_mem(char nome_arquivo[]);
-void escolher_arquivo_dat(char nome_arquivo[]);
+
+int escolher_arquivo_mem(char nome_arquivo[]);
+int escolher_arquivo_dat(char nome_arquivo[]);
 void leitura_arquivo_mem(int memoria[], char nome_arquivo[]);
 void leitura_arquivos_dados(int memoria_dados[], char nome_arquivo[]);
 void inicializar_registradores(int registradores[]);
-int ler_reg(int registradores[], int indice);
-void escrever_reg(int registradores[], int indice, int valor);
 
 int fetch(int memoria_instrucao[], int PC);
 struct decode campos(int instrucao);
 void execute(struct decode c, int registradores[], int memoria_dados[], int *PC, int *aritmeticas, int *memoria_acesso);
-void executar_programa(int memoria_instrucao[], int memoria_dados[], int registradores[]);
 int ULA(int A, int B, int controle, int *flag);
 
-void imprimir_memoria(int memoria[]);
+void imprimir_memoria_instrucoes(int memoria[]);
+void imprimir_memoria_dados(int memoria[]);
 void salvar_arquivo_dat(int memoria_dados[]);
 void salvar_asm(int memoria[]);
 
